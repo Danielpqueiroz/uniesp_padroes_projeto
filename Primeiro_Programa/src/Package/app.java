2e36668bf303efb1b665fbf34f86f76d01efccdf
Daniel pontes de queiroz <Danielqueiroz@academicos.com>
2023-08-21 20:59:06 -0300

    organizacao

--- Primeiro_Programa/src/Package/app.java
+++ Primeiro_Programa/src/Package/app.java
@@ -1,7 +1,10 @@
-package Primeiro_Programa;
+package Package;
 
 public class app {
+
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		// TODO Auto-generated method stub
+
 	}
+
 }
